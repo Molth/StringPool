11f39c84b6378ff726df0014241cefde8e1f714d
Molth Nevin <qq2201591870@163.com>
2025-05-17 00:51:36 +0800

    add a example with `ZString`

--- Examples/Examples/Program.cs
+++ Examples/Examples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Buffers;
+using Cysharp.Text;
 using NativeCollections;
 
 namespace Examples
@@ -6,6 +7,11 @@ namespace Examples
     internal sealed class Program
     {
         private static void Main()
+        {
+            Test2();
+        }
+
+        private static void Test1()
         {
             string a = StringPool.Shared.Rent("Big Herta".Length);
             NativeString str = NativeString.Create(a, 0);
@@ -16,6 +22,25 @@ namespace Examples
             string b = StringPool.Shared.Rent("Small Herta");
             Console.WriteLine(b);
             StringPool.Shared.Return(b);
+        }
+
+        private static void Test2()
+        {
+            using (var builder1 = ZString.CreateStringBuilder())
+            {
+                builder1.Append("Big Herta");
+                string a = StringPool.Shared.Rent(builder1.AsSpan());
+                Console.WriteLine(a);
+                StringPool.Shared.Return(a);
+            }
+
+            using (var builder2 = ZString.CreateStringBuilder())
+            {
+                builder2.Append("Small Herta");
+                string b = StringPool.Shared.Rent(builder2.AsSpan());
+                Console.WriteLine(b);
+                StringPool.Shared.Return(b);
+            }
         }
     }
 }
