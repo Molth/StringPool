46b1a7f96f69d6448e5404d4dfa877431b0ed273
Molth Nevin <qq2201591870@163.com>
2025-05-17 00:57:18 +0800

    Create TestStringPool3.cs

---
+++ Examples/Examples/Unity/TestStringPool3.cs
@@ -0,0 +1,30 @@
+﻿#if UNITY_2021_3_OR_NEWER
+using System;
+using System.Buffers;
+using System.Runtime.InteropServices;
+using Cysharp.Text;
+using TMPro;
+using UnityEngine;
+
+namespace Examples
+{
+    public sealed class TestStringPool3 : MonoBehaviour
+    {
+        public TMP_Text text;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                string a = StringPool.Shared.Rent(10000);
+                using Utf16ValueStringBuilder builder = ZString.CreateStringBuilder();
+                builder.Append("Herta");
+                ReadOnlySpan<char> span = a.AsSpan();
+                Span<char> span2 = MemoryMarshal.CreateSpan(ref MemoryMarshal.GetReference(span), span.Length);
+                builder.TryCopyTo(span2, out _);
+                text.text = a;
+            }
+        }
+    }
+}
+#endif
